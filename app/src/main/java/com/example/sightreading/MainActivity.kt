6d67faedcb99ee15b987e28a2560ff55b1322192
Ercan Öztürk <ercanozturk788@gmail.com>
2023-12-18 16:25:08 +0300

    Bass clef first version

--- app/src/main/java/com/example/sightreading/MainActivity.kt
+++ app/src/main/java/com/example/sightreading/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.sightreading
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sightreading.ui.theme.SightReadingTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,16 +43,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+
+
+    Column(modifier = Modifier
+        .padding(30.dp)
+        .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(30.dp))
+        Divider(color = Color.Black, thickness = 1.dp)
+        Spacer(modifier = Modifier.height(30.dp))
+        Divider(color = Color.Black, thickness = 1.dp)
+        Spacer(modifier = Modifier.height(30.dp))
+        Divider(color = Color.Black, thickness = 1.dp)
+        //Spacer(modifier = Modifier.height(30.dp))
+        Icon(
+            painter = painterResource(R.drawable.baseline_music_note_24),
+            contentDescription = "Music"
+        )
+        Divider(color = Color.Black, thickness = 1.dp)
+        Spacer(modifier = Modifier.height(30.dp))
+        Divider(color = Color.Black, thickness = 1.dp)
+        Spacer(modifier = Modifier.height(30.dp))
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:pixel_5")
 @Composable
 fun GreetingPreview() {
     SightReadingTheme {
         Greeting("Android")
     }
 }
+
